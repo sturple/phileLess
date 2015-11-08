11f4a6761d9bc9c76241edf57a406d90a7da47bb
Shawn Turple <shawn.turple@shaw.ca>
2015-11-08 12:12:49 -0800

    Create Plugin.php

---
+++ Classes/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Plugin Class
+*/
+namespace Phile\Plugin\Sturple\PhileLess;
+
+class Plugin extents \Phile\Plugin\AbstractPlugin implements \Phile\EventObserverInterface {
+  
+  public function __construct() {
+  
+  }
+  
+  public function on($eventKey, $data = null){
+  
+  }
+
+
+}
