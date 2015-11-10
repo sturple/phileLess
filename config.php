5d69bd1581e46750eb548d404d18396e267b6427
Shawn Turple <shawn.turple@shaw.ca>
2015-11-10 12:02:57 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,12 +2,13 @@
 /**
  * config file
  * see http://leafo.net/lessphp/docs/#compiling
+ * all file paths are relative to current theme.
  */
 /* lessjs, compressed, classic */
 return array(	
-	'active'			=>false,
+	'active'		=> false,
 	'formatter' 		=> 'lessjs', 
-	'comments'			=> false,
-	'inputFile' 		=> '/var/www/html/Phile/phile/themes/default/css/style.less',
-	'outputFile' 		=> '/var/www/html/Phile/phile/themes/default/css/style2.css',	
+	'comments'		=> false,
+	'inputFile' 		=> 'assets/css/style.less',
+	'outputFile' 		=> 'assets/css/style.css',	
 );
